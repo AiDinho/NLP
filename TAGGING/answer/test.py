b51f4f49d82279435215d77fd39c535a9c1b66be
Silvery Fu <silveryfu@gmail.com>
2014-03-04 19:26:08 -0800

    Delete test.

--- TAGGING/answer/test.py
+++
@@ -1,8 +0,0 @@
-from nltk.corpus import brown
-from nltk.probability import *
-
-cfd = ConditionalFreqDist()
-for sent in brown.tagged_sents():
-    for word, tag in sent:
-        if word =='run':
-            cfd['run']/inc(tag)
