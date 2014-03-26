efa6cfb6b80494cf713faa5f2cbf3eafa88541f2
Silvery <Silveryfu@gmail.com>
2014-03-26 16:15:53 -0700

    ..

--- CFG/ipv_vocab.py
+++ CFG/ipv_vocab.py
@@ -3,8 +3,8 @@
 import nltk
 
 try:
-    fvocab_in = open("./Vocab.temp","r")
-    fvocab_out = open("./Vocab_grail.gr","w")
+    fvocab_in = open("./Vocab.gr", "r")# notice, this file has been modified to fit the penn tag sets
+    #fvocab_out = open("./Vocab_grail.gr","w")
 except:
     print "Cannot open file."
 
@@ -18,15 +18,27 @@ for sent in nltk.corpus.webtext.sents('grail.txt'):
 cfd = nltk.ConditionalFreqDist(tagged_sents)    
 cfd_reverse = nltk.ConditionalFreqDist((tag, word) for (word, tag) in tagged_sents)
 
+    
 for line in fvocab_in:
-    word = line.split()[2]
-    keys = cfd[word].keys()
-    if keys == []:
-        fvocab_out.write("1    Misc    "+word+"\n")
+    if line.startswith("#") or line.startswith("\n"):
+        fvocab_out.write(line)        
+        continue
     else:
-        for key in keys:
-            fvocab_out.write(str(cfd[word][key])+"    "+key+"    "+word+"\n")
-
+        row = line.split()        
+        count = row[0]        
+        tag = row[1]        
+        word = row[2]
+        
+        if tag == "Misc":
+            keys = cfd[word].keys()
+            if keys == []:
+                fvocab_out.write("1    Misc    "+word+"\n")
+            else:
+                for key in keys:
+                    fvocab_out.write(str(cfd[word][key])+"    "+key+"    "+word+"\n") #TODO Hand tune the weights
+        else:
+            fvocab_out.write(str(1+cfd_reverse[tag][word])+"    "+tag+"    "+word+"\n")
+            
 print "...Finish!"
 fvocab_in.close()
 fvocab_out.close()
