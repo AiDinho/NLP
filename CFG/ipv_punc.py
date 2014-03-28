a7c860f0652ff94cbf21a40951734a23cf2b0444
Silvery Fu <silveryfu@gmail.com>
2014-03-28 08:35:00 -0700

    Add ipv_punc.py

---
+++ CFG/ipv_punc.py
@@ -0,0 +1,37 @@
+import nltk
+
+fbank = open("./atis3.treebank","r")
+fs1 = open("./Punc_S1.gr","w")
+
+print "...Generating..."
+
+rule_set = []
+productions = []
+
+for line in fbank:
+    tree = nltk.Tree(line)
+    nltk.Tree.chomsky_normal_form(tree)
+    productions = nltk.Tree.productions(tree)
+    rule_set = rule_set + productions
+
+n_rule_set = []
+tag_set = []
+count = 0
+for rule in rule_set:
+    s_rule = str(rule)    
+    s_rule = s_rule.replace("->"," ")
+    row = s_rule.split()
+    tag = row[0]
+    tag_set.append(tag)
+
+tag_set = list(set(tag_set))
+tag_set.sort()    # to make S1.gr looks nice
+
+
+for tag in tag_set:
+    print tag
+
+
+
+
+
