3447ac189d3a04de43193d2eef72bedbee3099e8
Silvery Fu <silveryfu@gmail.com>
2014-04-07 08:08:57 -0700

    Modifying align.

--- alignment/answer/align.py
+++ alignment/answer/align.py
@@ -10,7 +10,7 @@
 from __future__ import division
 from collections import defaultdict
 import optparse
-import sys
+import sys, math
 import os.path
 
 optparser = optparse.OptionParser()
@@ -33,28 +33,66 @@ bitext = [[sentence.strip().split() for sentence in pair] for pair in zip(open(f
 t_ef = defaultdict(float)
 c_ef = defaultdict(int)
 total_f = defaultdict(int)
-e_words = []
-f_words = []
+tol = pow(10,-4)  # the convergence tolerance
+de = defaultdict(int)   # store the counts of words        
+df = defaultdict(int)   # in a hash table staying efficient
+num = 0
 # permutate all the combinations of f -> e
-for (n, (f, e)) in enumerate(bitext):
-    for f_i in set(f):
-        f_words.append(f_i)
-        for e_j in set(e):
-            e_words.append(e_j)
-            t_ef[(e_j, f_i)] = 1
-
-e_words = list(set(e_words))
-f_words = list(set(f_words))
+for (f_sent, e_sent) in bitext:
+    for e_word in e_sent:
+        de[num,e_word] += 1
+    for f_word in f_sent:
+        df[num,f_word] += 1
+    num+=1
+    for f_i in set(f_sent):
+        for e_j in set(e_sent):
+            t_ef[e_j, f_i] = 1
 
 pairs = t_ef.keys()
 l = len(pairs)
 # initialize the t_ef, uniformly by default
 for (e, f) in t_ef.keys():
-    t_ef[(e, f)] = 1/l
+    t_ef[e, f] = 1/l
 
-for (e, f) in pairs:
-    c_ef[(e,f)] = 0
-    total_f[f] = 0
-    for (n, (f_sent, e_sent)) in enumerate(bitext):
-        for e_word in set(e_sent):
+curr_L = 0
+prev_L = tol + 1
+
+for i in range(5):
+    prev_L = curr_L
+    curr_L = 0
+    num = 0    
+    for (e, f) in pairs:
+        c_ef[e,f] = 0 
+        total_f[f] = 0
+    for (f_sent, e_sent) in bitext:
+        e_words = set(e_sent)     
+        f_words = set(f_sent)
+        for e_word in e_words:
+            n_e = de[num, e_word]
             total = 0
+            for f_word in f_words:
+                total += t_ef[e_word,f_word]*n_e
+            for f_word in f_words:
+                n_f = df[num, f_word]
+                rhs = t_ef[e_word,f_word]*n_e*n_f/total
+                c_ef[e_word,f_word] += rhs
+                total_f[f_word] += rhs
+        num+=1
+    for (e, f) in pairs:
+        t_ef[e,f] = c_ef[e,f]/total_f[f]
+        curr_L += math.log(t_ef[e,f],2)
+    print curr_L
+
+
+'''
+for (f_sent, e_sent) in bitext:
+    for (i, f) in enumerate(f_sent):
+        a_max = 0
+        for (j, e) in enumerate(e_sent):
+            if t_ef[(e,f)] > a_max:
+                a_max = t_ef[e,f]
+                j_max = j
+        sys.stdout.write("%i-%i " % (i,j_max))
+    sys.stdout.write("\n")  
+                
+'''
